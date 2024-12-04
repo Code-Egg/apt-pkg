c20f16dacc4e94b7cf4a56b82cf001107ba5ffe5
Code-Egg <eric780217@gmail.com>
2024-12-04 13:42:47 +0800

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -13,27 +13,18 @@ product=$1
 version=$2
 revision=$3
 dists=$4
-input_archs=$5
-build_flag=$6
-push_flag=$7
-sync_flag=$8
-edge_flag=$9
-
+input_archs=amd64
+build_flag=build
 lsapi_version=8.1
-
-dists=noble
 set_archs=amd64
 
 check_input
 set_paras
-
-if [ $build_flag == build ]; then
-        set_build_dir
-        prepare_source
-        pbuild_packages
-        echo " ################# End of Result #################"
-        echo $(date)
-fi
+set_build_dir
+prepare_source
+pbuild_packages
+echo " ################# End of Result #################"
+echo $(date)
 
 
 

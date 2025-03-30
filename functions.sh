48790fd099c20ee4617eadeddf649403bf1ae914
Code-Egg <eric780217@gmail.com>
2025-03-30 21:39:57 +0800

    modified:   .github/workflows/self-host-build.yml 	modified:   functions.sh

--- functions.sh
+++ functions.sh
@@ -236,7 +236,11 @@ upload_to_local(){
     cd $BUILD_DIR/build-result
     echo "Upload deb files to local"
     for dist in `echo $dists`; do
-        cp --update=none $BUILD_DIR/build-result/${dist}/*.deb /home/runner/results/debian/pool/main/${dist}/
+    echo 'D1'
+        ls /home/runner/results/debian/pool/main/
+        echo 'D2'
+        ls /home/
+        #cp --update=none $BUILD_DIR/build-result/${dist}/*.deb /home/runner/results/debian/pool/main/${dist}/
     done
     echo "End of uploading files to local"
 }

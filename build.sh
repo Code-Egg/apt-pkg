936432c6e5562185074573f22869db51525f8f1f
Code-Egg <eric780217@gmail.com>
2025-04-11 15:46:00 +0800

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -31,7 +31,7 @@ fi
 lsapi_version=8.1
 if [ $dists = "all" ]; then
         echo " convert dists value "
-            dists="jammy noble"
+            dists="noble jammy"
             #dists="jammy bionic buster focal bullseye bookworm noble"
         echo " the new value for dists is $dists "
 fi

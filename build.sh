9154d08f31a4c5c67c7f5ec6fd332bdc6c668410
Code-Egg <eric780217@gmail.com>
2025-04-11 13:17:47 +0800

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -13,14 +13,21 @@ dists=$2
 input_archs=$3
 build_flag=$4
 release_flag=$5
-version="grep ${product} VERSION.txt | awk -F '=' '{print $2}'"
-revision=$(curl -isk https://${target_server}/debian/pool/main/$dists/ | grep ${product}_${version} \
-  | awk -F '-' '{print $4}' | awk -F '+' '{print $1}' | tail -1)
-if [ $revision == ?(-)+([[:digit:]]) ]; then
-    revision=$((revision+1))
-else
-    revision=1
+
+if [ -z "${version}" ]; then
+    version="grep ${product}= VERSION.txt | awk -F '=' '{print $2}'"
 fi
+if [ -z "${revision}" ]; then
+    revision=$(curl -isk https://${target_server}/debian/pool/main/$dists/ | grep ${product}_${version} \
+      | awk -F '-' '{print $4}' | awk -F '+' '{print $1}' | tail -1)
+    if [ $revision == ?(-)+([[:digit:]]) ]; then
+        revision=$((revision+1))
+    else
+        echo "$revision is not a number, set value to 1"
+        revision=1
+    fi      
+fi      
+
 lsapi_version=8.1
 #if [ $dists = "ALL" ]; then
 #        echo " convert dists value "
